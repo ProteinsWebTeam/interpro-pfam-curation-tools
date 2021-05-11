2af1f9c2b17baabb7ed1a5b190f893e3d7df81b4
typhainepl <typhaine@ebi.ac.uk>
2021-05-11 15:55:24 +0100

    refactor COGs analysis pipeline

--- cogs_analysis/README.md
+++ cogs_analysis/README.md
@@ -1,10 +1,24 @@
 # JACCARD index calculation between COGs and integrated InterPro entries
 
-The process is divided into 5 different steps
+## Requirements:
 
-1) The representative list of proteins from UNIPROT is loaded in a redis hash (`get_rp_proteins.py`)
+Python 3.6+, with package `cx_Oracle`, `redis`
 
-2) For each protein in the representative list (`IPR_RP_AC.py`):
+## Configuration
+
+This pipeline expect an INI config file.
+Load Oracle environment: `source ~oracle/ora112setup.sh`
+
+## Usage
+
+The process is divided into 5 different steps, that can be run individually.
+
+To run the full process (except the last step) use the following command line `python run_cog_analysis.py config.ini -1 -2 -3 -4`
+
+
+1) The representative list of proteins from UNIPROT is loaded in a redis hash
+
+2) For each protein in the representative list:
 	
 	For each integrated InterPro entry, we calculated the midpoint of the different fragments and count the number of protein matching
     
@@ -12,9 +26,9 @@ The process is divided into 5 different steps
 	- protein_hash[protein_acc]={"IPR1":[midpoint1,midpoint2], "IPR2":[midpoint1]}
 	- ipr_count[ipr]=count_protein
 
-3) Get the list of COGs into a redis queue which will be used by 8 different jobs submitted in parallel for step 4 (`get_cog_list.py`)
+3) Get the list of COGs into a redis queue which will be used by 8 different jobs submitted in parallel for step 4
 
-4) For each COG (`search_intersect.py`): 
+4) For each COG: 
 	
 	For each protein:
 	- Get IPR and midpoints
@@ -24,6 +38,7 @@ The process is divided into 5 different steps
 	cog, ipr, ji, jc1, jc2, intersect, union, totalprotein_cog, totalprotein_ipr
 
 5) Concat all the results files into a unique file
+`cat <OUTPUT_FILE_DIR>/* > COGs_results`
 
 
 
--- cogs_analysis/search_intersect.py
+++ cogs_analysis/search_intersect.py
@@ -1,15 +1,9 @@
-import cx_Oracle
-import redis
 import argparse
 import json
+import os
+from configparser import ConfigParser
 
-
-def redis_connection():
-    # Connection to REDIS
-    REDIS_HOST = "ebi-pdbe.ebi.ac.uk"
-    REDIS_PORT = "54321"
-
-    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+from cogs_analysis.utils import closeConnection, dbConnection, redisConnection
 
 
 def isIntersect(midpoint, start, end):
@@ -19,27 +13,40 @@ def isIntersect(midpoint, start, end):
         return 0
 
 
-def main():
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", metavar="FILE", help="configuration file", required=True)
+    parser.add_argument("output", help="name of the output file for the current job")
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.config):
+        parser.error(f"cannot open '{args.config}': " f"no such file or directory")
+
+    config = ConfigParser()
+    config.read(args.config)
+
+    args = parser.parse_args()
+
     # redis connection
-    server = redis_connection()
+    server = redisConnection(config)
+    redis_cog = config["redis"]["redis_cog"]
+    rp_proteins = config["redis"]["redis_rp"]
+    protein_list = config["redis"]["redis_protein"]
+    ipr_count = config["redis"]["redis_ipr_count"]
 
     # iptst connection
-    dsn = cx_Oracle.makedsn("***REMOVED***", "***REMOVED***", 'iptst')
-    connection_cog = cx_Oracle.connect(
-        user='***REMOVED***', password='***REMOVED***', dsn=dsn)
-    # connection_cog = cx_Oracle.connect('ops$hychang/changeme@iptst')
-    dbcursor_cog = connection_cog.cursor()
+    connection_cog, dbcursor_cog = dbConnection(config, "iptst")
 
     cog = 1
 
     while cog:
         # get IPR/signature from the REDIS_QUEUE
-        cog = server.lpop(ARGS.redis_cog)
+        cog = server.lpop(redis_cog)
 
         if not cog:
             break
 
-        cog = str(cog.decode('utf8'))
+        cog = str(cog.decode("utf8"))
 
         # get common protein accessions and intersect between COGs and IPR
         print("Searching for intersecting proteins betwwen COGS and InterPro")
@@ -53,7 +60,7 @@ def main():
 
         for row in member1list:
             protein, cog, start, end = row
-            if server.hexists(ARGS.rp_proteins, protein):
+            if server.hexists(rp_proteins, protein):
                 # initialize protein count for cogs
                 if cog in cogs_count:
                     cogs_count[cog].add(protein)
@@ -61,9 +68,8 @@ def main():
                     cogs_count[cog] = set([protein])
 
                 # if the protein in found with at least one IPR
-                if server.hexists(ARGS.protein_list, protein):
-                    list_ipr = json.loads(server.hget(
-                        ARGS.protein_list, protein).decode('utf8'))
+                if server.hexists(protein_list, protein):
+                    list_ipr = json.loads(server.hget(protein_list, protein).decode("utf8"))
 
                     # for each IPR matching the protein, searching if they intersect with the current COG
                     for ipr in list_ipr:
@@ -79,41 +85,27 @@ def main():
                                 break
 
         print("Saving results in file")
-        with open(ARGS.output, 'a') as file_to_print:
+        with open(args.output, "a") as file_to_print:
 
             for key in sorted(intersect_dict.keys()):
                 intersect = len(intersect_dict[key])
-                (cog, ipr) = key.split('_')
+                (cog, ipr) = key.split("_")
 
                 set1 = len(cogs_count[cog])
-                set2 = int(server.hget(
-                    ARGS.ipr_count, ipr).decode('utf-8'))
+                set2 = int(server.hget(ipr_count, ipr).decode("utf-8"))
 
-                union = set1+set2-intersect
+                union = set1 + set2 - intersect
 
-                ji = round(intersect/union, 2)
-                jc1 = round(intersect/float(set1), 2)
-                jc2 = round(intersect/float(set2), 2)
+                ji = round(intersect / union, 2)
+                jc1 = round(intersect / float(set1), 2)
+                jc2 = round(intersect / float(set2), 2)
 
-                file_to_print.write("{},{},{},{},{},{},{},{},{}\n".format(
-                    cog, ipr, ji, jc1, jc2, intersect, union, set1, set2))
+                file_to_print.write(
+                    "{},{},{},{},{},{},{},{},{}\n".format(
+                        cog, ipr, ji, jc1, jc2, intersect, union, set1, set2
+                    )
+                )
 
     print("Calculation completed")
+    closeConnection(connection_cog, dbcursor_cog)
 
-
-if (__name__ == '__main__'):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--redis_cog", help='name of the redis queue for cogs accessions', default='jaccard_cogs')
-    parser.add_argument(
-        "--rp_proteins", help='name of the redis queue for representative proteins', default='jaccard_cog_rp')
-    parser.add_argument(
-        "--protein_list", help='name of the redis queue for ipr and midpoints per protein', default='jaccard_cogs_prot')
-    parser.add_argument(
-        "--ipr_count", help='name of the redis queue for total number of proteins per ipr', default='jaccard_cog_ipr')
-    parser.add_argument(
-        "output", help='name of the output file for the current job')
-
-    ARGS = parser.parse_args()
-
-    main()
